a1ce26f3833c99a3992af821e7cafcd21705ab78
chepeniv <jose.olmos@atlasschool.com>
2024-07-19 15:41:32 -0500

    [NA] notetaking; added 'include' lines

--- main.h
+++ main.h
@@ -1,8 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <dirent.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <fcntl.h>
 
 #endif
