1d60785bef28970e2d58740b8d9bc1dff314b1b2
chepeniv <jose.olmos@atlasschool.com>
2024-07-20 16:33:53 -0500

    [passing] created super simple shell

---
+++ test/super_simple_schell.exr.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/*simplify program if possible */
+int main()
+{
+	unsigned int i, len;
+	size_t n;
+	ssize_t status;
+	char *line = NULL;
+	char *linedup = NULL;
+	char *word = NULL;
+	char **input = NULL;
+	char **next = NULL;
+	int *forkstatus;
+	pid_t this_proc, child_proc;
+
+	do
+	{
+		printf("$$$hell :: ");
+		status = getline(&line, &n, stdin);
+
+		/*count delimeted substrings*/
+		len = 1;
+		linedup = strdup(line);
+		word = strtok(linedup, " \t\n");
+		if (!strcmp(word, "exit"))
+		{
+			free(linedup);
+			printf("exiting...\n");
+			break;
+		}
+		while (word != NULL)
+		{
+			word = strtok(NULL, " \t\n");
+			len++;
+		}
+
+		/*allocate mem for array*/
+		/*construct array */
+		input = malloc(sizeof(void *) * len);
+		*input = strtok(line, " \t\n");
+		next = input;
+		while (*next != NULL)
+		{
+			next++;
+			*next = strtok(NULL, " \t\n");
+		}
+
+		child_proc = fork();
+		if (child_proc < 0)
+		{
+			perror("ERROR:");
+			return (1);
+		}
+		else if (child_proc == 0)
+		{
+			if (execve(*input, input, NULL) == -1)
+				perror("ERROR:");
+			break;
+		}
+		else
+			wait(forkstatus);
+
+		/*free mem*/
+		free(linedup);
+		free(input);
+
+	} while (status > -1);
+
+	free(line);
+	return (0);
+}
+
