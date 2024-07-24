4b3598b0b2b22f7f9ff787d9d97528166d58e50b
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-24 15:12:51 -0500

    Switching over to simpler code, backed up main.

--- test/Nathan_shell_idea.c
+++ test/Nathan_shell_idea.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
+#include "main.h"
 
 #define MAX_ARGS 128
 #define PATH_PREFIX "/bin/"
@@ -73,27 +67,48 @@ char **create_tok_array(char *inputline, char *delims, int toklen)
 
 char *get_path(char *cmdname)
 {
-	char *path_env = getenv("PATH");
-	char *path = strdup(path_env); /* copy to avoid modifying original*/
-	char *dir = strtok(path, ":"); /*Split PATH into directories*/
+	char **env = environ;
+	char *path_value = NULL;
+	char *path;
+	char *dir;
 	char *cmdpath;
 	struct stat file_stat;
 
+	while (*env)
+	{
+		if (strncmp(*env, "PATH=", 5) == 0)
+		{
+			path_value = *env + 5;
+			break;
+		}
+		env++;
+	}
+
+	if (!path_value)
+	{
+		return (NULL);
+	}
+
+	path = strdup(path_value);
+
+	dir = strtok(path, ":");
 	while (dir != NULL)
 	{
-		cmdpath = malloc(strlen(dir) + strlen(cmdname) + 2); /* +2 for '/' and '\0' */
+		cmdpath = malloc(strlen(dir) + strlen(cmdname) + 2);
 		sprintf(cmdpath, "%s/%s", dir, cmdname);
+		cmdpath[strlen(dir) + strlen(cmdname) + 1] = '\0';
+
 		if (stat(cmdpath, &file_stat) == 0)
 		{
 			free(path);
-			return cmdpath;
+			return (cmdpath);
 		}
 		free(cmdpath);
 		dir = strtok(NULL, ":");
 	}
 
 	free(path);
-	return NULL;
+	return (NULL);
 }
 
 /**
@@ -120,24 +135,52 @@ int run_cmd(char *cmdpath, char **token_array)
 {
 	struct stat file_stat;
 	pid_t child_proc;
+	char **args = NULL;
+	int i;
 
-	if (stat(cmdpath, &file_stat) == 0)
+	if (stat(cmdpath, &file_stat) == 0 && (file_stat.st_mode & S_IXUSR))
 	{
 		child_proc = fork();
 		if (child_proc < 0)
 		{
-			perror("ERROR:");
-			return (1);
+			perror("fork");
+			return 1;
 		}
 		else if (child_proc == 0)
 		{
-			if (execve(cmdpath, token_array, NULL) == -1)
-				perror("ERROR:");
-			return (-1);
+
+			args = malloc(sizeof(char *) * (MAX_ARGS + 1));
+			if (!args)
+			{
+				perror("malloc");
+				exit(EXIT_FAILURE);
+			}
+
+			args[0] = cmdpath; /* First argument is the command itself */
+			for (i = 1; token_array[i - 1] != NULL && i < MAX_ARGS; i++)
+			{
+				args[i] = token_array[i - 1];
+			}
+			args[i] = NULL;
+
+			if (execve(cmdpath, args, NULL) == -1)
+			{
+				perror(token_array[0]);
+				exit(EXIT_FAILURE);
+			}
 		}
 		else
-			wait(&child_proc);
+		{
+			wait(NULL);
+		}
 	}
+	else
+	{
+		fprintf(stderr, "%s: command not found\n", token_array[0]);
+		return (1);
+	}
+	free(args);
+	return (0);
 }
 
 /**
@@ -147,41 +190,33 @@ int run_cmd(char *cmdpath, char **token_array)
  */
 int main(void)
 {
-	char *inputline = NULL;
+	char *inputline = NULL, **token_array = NULL;
 	size_t n = 0;
 	ssize_t read;
-	char **token_array = NULL;
 	struct stat file_stat;
-	int status;
-	int toklen;
+	int status, toklen;
 
-	printf("--------\n");
-	printf("Welcome to Atlas Simple Shell!\n");
-	printf("Go away\n");
-	printf("--------\n");
-
-	while (1)
+	if (isatty(STDIN_FILENO))
 	{
-		printf("$$ ");
-		read = getline(&inputline, &n, stdin);
-		if (read == -1)
-		{
-			if (feof(stdin))
-			{
-				break;
-			}
-			else
-			{
-				perror("getline");
-				exit(EXIT_FAILURE);
-			}
-		}
+		printf("--------\n");
+		printf("Welcome to Atlas Simple Shell!\n");
+		printf("Go away\n");
+		printf("--------\n");
+	}
 
-		inputline[strcspn(inputline, "\n")] = 0;
+	while ((read = getline(&inputline, &n, stdin)) != -1)
+	{
+		if (read > 0 && inputline[read - 1] == '\n')
+			inputline[read - 1] = '\0';
+
 		toklen = count_tokens(inputline, " \t\n");
 		token_array = create_tok_array(inputline, " \t\n", toklen);
 
-		/* Handle empty input or "exit" command */
+		if (toklen == 1)
+		{
+			continue; /* Skip to next iteration if empty command */
+		}
+
 		if (token_array[0] == NULL || strcmp(token_array[0], "exit") == 0)
 		{
 			break;
@@ -193,7 +228,7 @@ int main(void)
 		else
 		{
 			char *cmdpath = get_path(token_array[0]);
-			if (stat(cmdpath, &file_stat) == 0)
+			if (stat(cmdpath, &file_stat) == 0 && (file_stat.st_mode & S_IXUSR))
 			{
 				status = run_cmd(cmdpath, token_array);
 				if (status == -1)
@@ -208,6 +243,7 @@ int main(void)
 
 			free(cmdpath);
 		}
+
 		free(token_array);
 	}
 
