655cf0c1b8a9fe843fd2171eed1363cabd4663c9
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-24 14:32:29 -0500

    Updated include

--- main.c
+++ main.c
@@ -1,10 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
 #include "main.h"
 
 #define MAX_ARGS 128
