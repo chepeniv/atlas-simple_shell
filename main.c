47a81bcde6fbeced909cfc2dff00964f239c6ab3
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-25 19:44:58 -0500

    Reverted

--- main.c
+++ main.c
@@ -10,33 +10,30 @@
 int main(int argc, char **argv)
 {
 	unsigned int toklen;
-	size_t n = 0;
+	size_t n;
 	char *inputline = NULL;
 	char *delims = " \t\n";
 	char *cmdname = NULL;
 	char **token_array = NULL;
 	char *cmdpath;
-	int status = 0;
+	int status;
 
 	if (argc > 1)
 	{
 		/* Non-interactive mode: Execute a single command from arguments */
 		cmdname = argv[1];
 		cmdpath = get_path(cmdname);
-		if (!cmdpath)
-		{
-			fprintf(stderr, "%s: command not found\n", cmdname);
-			return (1);
-		}
-		status = run_cmd(cmdpath, &argv[1]);
+		token_array = &argv[1];
+		run_cmd(cmdpath, token_array);
 		free(cmdpath);
-		return (status);
+
+		return (0);
 	}
 	else
 	{
 		/* Interactive mode */
 		if (isatty(STDIN_FILENO))
-		{ /* check if it's interactive mode*/
+		{
 			printf("--------\n");
 			printf("Welcome to Atlas Simple Shell!\n");
 			printf("Go away\n");
@@ -45,68 +42,40 @@ int main(int argc, char **argv)
 
 		do
 		{
-			/* print the prompt only in interactive mode */
+			/* Display prompt and read input line */
 			if (isatty(STDIN_FILENO))
 			{
 				printf("$$ ");
 			}
 
 			status = getline(&inputline, &n, stdin);
-			if (status == -1)
-			{
-				break;
-			}
 
+			/* Tokenize input line and get command name */
 			toklen = count_tokens(inputline, delims);
 			token_array = create_tok_array(inputline, delims, toklen);
-
-			if (token_array == NULL)
-			{
-				perror("Error: ");
-				continue;
-			}
-
-			/* Check for empty command */
-			if (toklen == 1)
-			{
-				continue;
-			}
-
-			cmdname = token_array[0];
-			if (!cmdname)
-			{
-				free(token_array);
-				continue;
-			}
+			cmdname = *token_array;
 
 			/* Exit if the 'exit' command is entered */
-			if (strcmp(cmdname, "exit") == 0)
+			if (!strncmp(cmdname, "exit", 4))
 			{
 				free(inputline); /* Free allocated memory for inputline */
 				free(token_array);
 				return (0);
 			}
 
+			/* Get full path of command and execute it */
 			cmdpath = get_path(cmdname);
 			if (cmdpath)
 			{
 				status = run_cmd(cmdpath, token_array);
-				if (status == 127)
-				{
+				if (status == -1)
 					fprintf(stderr, "%s: command not found\n", token_array[0]);
-				}
 				free(cmdpath);
 			}
-			else
-			{
-				fprintf(stderr, "%s: command not found\n", token_array[0]);
-				status = 1;
-			}
 			free(token_array);
-
-		} while (status != -1);
-
-		free(inputline);
-		return (status);
+		} while (status != -1); /* Continue until an error occurs */
 	}
+
+	free(inputline);
+	return (0);
 }
