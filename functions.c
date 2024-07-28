ef7a2a8e8542d48072de56e4cbb75ac9d23c9262
chepeniv <jose.olmos@atlasschool.com>
2024-07-28 02:02:08 -0500

    [NA] added comments to functions

--- functions.c
+++ functions.c
@@ -12,6 +12,10 @@ int count_tokens(char *inputline, char *delims)
 	int len = 0;
 	char *linedup, *token;
 
+	/*
+	 * because strtok destroys the string it operates on
+	 * we malloc-duplicate a dummy
+	 */
 	linedup = strdup(inputline);
 	token = strtok(linedup, delims);
 
@@ -25,7 +29,6 @@ int count_tokens(char *inputline, char *delims)
 	return (len);
 }
 
-
 /**
  * create_tok_array - Creates an array of tokens from a string.
  * @inputline: The input string.
@@ -38,9 +41,11 @@ char **create_tok_array(char *inputline, char *delims, int toklen)
 {
 		char **token, **array;
 
+		/* initialize the array */
 		array = malloc(sizeof(void *) * toklen);
 		*array = strtok(inputline, delims);
 
+		/* add tokes to the array untill NULL is returned */
 		token = array;
 		while (*token != NULL)
 		{
@@ -49,7 +54,6 @@ char **create_tok_array(char *inputline, char *delims, int toklen)
 		}
 		return (array);
 }
-
 
 /**
  * get_path - Gets the full path of a command.
@@ -62,16 +66,22 @@ char *get_path(char *cmdname)
 	int cmdlen;
 	char *cmdpath;
 
+	/*
+	 * special cases where command given is either
+	 * an absolute path or a relative path
+	 */
 	if (cmdname[0] == '/' || cmdname[0] == '.')
 	{
 		cmdpath = strdup(cmdname);
 		return (cmdpath);
 	}
+	/* special env command given */
 	else if (!strcmp(cmdname, "env"))
 	{
 		cmdpath = strdup(cmdname);
 		return (cmdpath);
 	}
+	/* otherwise construct a fullpath for the command given */
 	cmdlen = strlen(cmdname);
 	cmdpath = malloc(sizeof(char) * (cmdlen + 6));
 	cmdpath = strcpy(cmdpath, "/bin/");
@@ -92,23 +102,25 @@ int run_cmd(char *cmdpath, char **token_array)
 	struct stat file_stat;
 	pid_t child_proc;
 
+	/* execute _env() for special env command given */
 	if (!strcmp(cmdpath, "env"))
 		_env();
-	else if (stat(cmdpath, &file_stat) == 0)
+	/* otherwise ask operating system to run the command */
+	else if (stat(cmdpath, &file_stat) == 0) /*ensure the executable exist */
 	{
-		child_proc = fork();
+		child_proc = fork(); /*create subprocess*/
 		if (child_proc < 0)
 		{
 			perror("ERROR: failed to fork()\n");
 			return (-1);
 		}
 		else if (child_proc == 0)
-		{
+		{	/* replace the subprocess with the exec program */
 			if (execve(cmdpath, token_array, NULL) == -1)
 				perror("ERROR: failed to create child proc\n");
 			return (-1);
 		}
-		else
+		else /* parent process waits for the subprocess to conclude */
 			wait(&child_proc);
 	}
 	return (0);
@@ -124,9 +136,11 @@ char **_env(void)
 	int i = 0;
 	char **env;
 
+	/* copy system's environ array onto our programs array */
 	env = environ;
 	while (env[i])
 	{
+		/* output the contents of array to stdout */
 		printf("%s\n", env[i]);
 		i++;
 	}
